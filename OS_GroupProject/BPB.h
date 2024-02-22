6dec0ccc304c3dc732c8d7c9377683aefdee5348
DangTrieuKha <126672405+DangTrieuKha@users.noreply.github.com>
2024-02-22 17:24:33 +0700

    update BPB class for NTFS

---
+++ OS_GroupProject/BPB.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "StaticVariable.h"
+#include "VolumeBootRecord.h"
+#include "SectorReader.h"
+class BPB : public VolumeBootRecord
+{
+private:
+    std::wstring driveName;
+    uint32_t BytePerSector;    // 0xB 2 byte
+    uint32_t SectorPerCluster; // 0xD->0xE
+    uint32_t MFTsize;           // 0x40->0x40  the true size is 2^abs(MFTsize) -> luu chinh xac roi
+    uint64_t SectorVolume;     // 0x28->0x2F
+    uint64_t StartMFTCluster;  // 0x30->0x37
+};
+
--- OS_GroupProject/NTFS.h
+++ OS_GroupProject/NTFS.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "StaticVariable.h"
 #include "FileManagementSystem.h"
+#include "BPB.h"
+
 using namespace std;
 
 
@@ -44,12 +46,7 @@ public:
     bool returnPreviousDirectory() override;
 
 private:
-    std:: wstring driveName;
-    uint32_t BytePerSector;    // 0xB 2 byte
-    uint32_t SectorPerCluster; // 0xD->0xE
-    uint32_t MFTsize;           // 0x40->0x40  the true size is 2^abs(MFTsize) -> luu chinh xac roi
-    uint64_t SectorVolume;     // 0x28->0x2F
-    uint64_t StartMFTCluster;  // 0x30->0x37
+    shared_ptr<BPB> bootSector;
     // std::vector<Record> ListRecord;
 
 
